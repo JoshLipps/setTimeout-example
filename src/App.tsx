4d5e7da88ff832ca7c865422facb2120ca9790ef
Josh Lipps <josh.lipps@gmail.com>
2021-03-18 15:39:16 +0900

    Refactored into custom Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,18 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-let i = 0;
+import usePoll from './hooks/usePoll';
+
 const  App = () => {
   const [img, setImg] = useState(logo);
-  const timeoutIDRef = useRef<ReturnType<typeof setTimeout>  | null>(null);
-  const canceled = useRef<boolean>(false);
 
-  const updateImg = useCallback(async()=>{
-    const iteration = i;
-    i += 1;
-    if(timeoutIDRef.current !== null){
-      console.log('Clearing previous',iteration, timeoutIDRef.current);
-      clearTimeout(timeoutIDRef.current);
-      timeoutIDRef.current = null;
-    }
-    console.log('UpdatingImage (Long wait)', iteration);
+  usePoll(async()=>{
+    console.log('UpdatingImage (Long wait)');
     setImg(`${logo}?v=${Date.now()}`);
     //Fake the download
     await wait(5000);
+  }, 1000);
 
-
-    //Start next one in the chain
-    if(!canceled.current){
-      console.log('Starting next timeout', iteration);
-      timeoutIDRef.current = setTimeout(updateImg, 1000);
-    }
-  },[]);
-
-  useEffect(()=>{
-
-    updateImg();
-
-    return () => {
-      console.log('canceled');
-      canceled.current = true;
-      if(timeoutIDRef.current !== null){
-        console.log('clearing final', timeoutIDRef.current);
-        clearTimeout(timeoutIDRef.current);
-        timeoutIDRef.current = null;
-      }
-    }
-  },[updateImg])
 
   return (
     <div>
